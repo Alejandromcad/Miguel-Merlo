d2eefe8e9ccafa0c96b215a9ff37b1beb11898b3
Miguel Merlo <miguealemer@gmail.com>
2018-04-26 06:49:13 +0000

    added contact logic, and final touches

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require("express"),
 app         = express(),
+bodyParser  = require("body-parser"),
 flash       = require("connect-flash");
 
 // APP CONFIGURATION
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(flash());
 console.log(__dirname);
@@ -13,7 +15,6 @@ var indexRoutes = require("./routes/index.js");
 
 // USING ROUTES
 app.use("/", indexRoutes);
-
 
 
 app.listen(process.env.PORT, process.env.IP, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
-var router  = express.Router();
+var router  = express.Router({mergeParams: true});
+var nodemailer = require("nodemailer");
 
 
 // HOME PAGE
@@ -8,12 +9,38 @@ router.get("/", (req, res) =>{
 });
 
 // CONTACT
-
 router.get("/contact", (req, res) =>{
     res.render("contact");
 });
+router.post("/contact", (req, res) => {
+    // takes data from the form and sends it to my gmail
+      var smtpTrans = nodemailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.GMAIL_USER || "miguealemer@gmail.com",
+            pass: process.env.GMAIL_PASS || "130499mix"
+        }
+      });
 
+        var mailOptions = {
+          from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+          to:   process.env.GMAIL_USER || "miguealemer@gmail.com",
+          subject: "New message from contact form at MiguelMerlo.com",
+          text: `${req.body.name} (${req.body.email}) dice: ${req.body.message}`
+        };
 
+         smtpTrans.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+            res.redirect("/contact");
+          }
+        });
+        
+});
 module.exports = router;
 
 
